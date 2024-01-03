7a772a8cd1933b9c9a788299b71d2aef2338d6f9
WardTheProgrammer <nijhuis.ward@gmail.com>
2024-01-03 19:39:25 +0100

    Write a function that plays a single round of Rock Paper Scissors The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round. Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation). Account for TIES by re-playing the round.

--- RPS.js
+++ RPS.js
@@ -2,17 +2,33 @@ function getComputerChoice() {
     randumNum = Math.floor(Math.random()*3)
     switch (randumNum) {
         case 0:
-            return "Rock";
+            return "rock";
             break;
         case 1:
-            return "Paper";
+            return "paper";
             break;
         case 2:
-            return "Scissors";
+            return "scissors";
             break;
         default:
-            return "Rock";
-    }
-}
+            return "rock";
+    };
+};
 
-console.log(getComputerChoice())
+const playerSelection = "Rock";
+let computerSelection = getComputerChoice();
+
+function generateWinner(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
+        computerSelection = getComputerChoice();
+        generateWinner(playerSelection, computerSelection);
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors" || playerSelection.toLowerCase() === "paper" && computerSelection === "rock" || playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+        console.log(`You win! ${playerSelection.toLowerCase()} beats ${computerSelection}`);
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper" || playerSelection.toLowerCase() === "paper" && computerSelection === "scissors" || playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+        console.log(`You lose! ${computerSelection.toLowerCase()} beats ${playerSelection}`);
+    } else {
+        console.log("Something went wrong!");
+    };
+};
+
+generateWinner(playerSelection, computerSelection);
