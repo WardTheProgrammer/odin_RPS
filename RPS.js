c0d4fed331c313286b3cbcb3c456ff82b3ea3671
WardTheProgrammer <nijhuis.ward@gmail.com>
2024-01-06 16:08:32 +0100

    Run a full game of Rock Paper Scissors
    Write a NEW function called game(). Use the previous function inside of this one to play a best-of-five game that keeps score and reports a winner or loser at the end.Use prompt() to get input from the user.

--- RPS.js
+++ RPS.js
@@ -15,23 +15,52 @@ function getComputerChoice() {
     };
 };
 
-const playerSelection = "Rock";
-let computerSelection = getComputerChoice();
 
-function generateWinner(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === computerSelection) {
+
+function generateWinner(i) {
+    let playerSelection = prompt(`Round ${i+1}:  Rock, Paper, Scissors, Shoot:`);
+    let computerSelection = getComputerChoice();
+    if (playerSelection.toLowerCase() == computerSelection) {
         console.log("It is a tie!");
-        computerSelection = getComputerChoice();
-        generateWinner(playerSelection, computerSelection);
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors" || playerSelection.toLowerCase() === "paper" && computerSelection === "rock" || playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
-        console.log(`You win! ${playerSelection.toLowerCase()} beats ${computerSelection}`);
-        return true;
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper" || playerSelection.toLowerCase() === "paper" && computerSelection === "scissors" || playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
-        console.log(`You lose! ${computerSelection.toLowerCase()} beats ${playerSelection}`);
-        return false;
+        return generateWinner();
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection === "scissors" || playerSelection.toLowerCase() === "paper" && computerSelection === "rock" || playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+    console.log(`You win the round! ${playerSelection.toLowerCase()} beats ${computerSelection}`);
+    return true;
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection === "paper" || playerSelection.toLowerCase() === "paper" && computerSelection === "scissors" || playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+    console.log(`You lose the round! ${computerSelection} beats ${playerSelection.toLowerCase()}`);
+    return false;
+    } else {
+        console.log("Something went wrong!");
+        let playerSelection = prompt(`Round ${i+1}:  Rock, Paper, Scissors, Shoot:`);
+        let computerSelection = getComputerChoice();
+        return generateWinner();
+    };
+};
+
+function game() {
+    let playerScore = 0
+    let computerScore = 0
+    for (let i = 0; i < 5; i++) {
+        let result = generateWinner(i);
+        if (result === true) {
+            playerScore += 1;
+        } else if (result === false) {
+            computerScore += 1;
+        } else {
+            console.log("Something went wrong!"); 
+        };
+    };
+    if (playerScore > computerScore) {
+        console.log("You win the game!");
+        console.log(`Your points: ${playerScore}`);
+        console.log(`The computers points: ${computerScore}`);
+    } else if (playerScore < computerScore) {
+        console.log("You lose the game!");
+        console.log(`Your points: ${playerScore}`);
+        console.log(`The computers points: ${computerScore}`);
     } else {
         console.log("Something went wrong!");
     };
 };
 
-generateWinner(playerSelection, computerSelection);
+game();
